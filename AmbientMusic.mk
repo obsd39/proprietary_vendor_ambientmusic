7b08dc8d80a1cbeb5e8917df69c73f5eaf1e5203
Christian Oder <myself5@carbonrom.org>
2019-03-29 20:41:37 +0100

    ambientmusic: Move Sourcecode to external/google

--- AmbientMusic.mk
+++
@@ -1,7 +0,0 @@
-DEVICE_PACKAGE_OVERLAYS += vendor/ambientmusic/overlay
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/ambientmusic/overlay
-
-PRODUCT_PACKAGES += \
-    SystemUIGoogle
-
-include  vendor/ambientmusic/ambient/ambient-vendor.mk
