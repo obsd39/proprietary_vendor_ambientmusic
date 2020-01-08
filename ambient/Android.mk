990f2e63bce5ecae575c68c3c4d51034a0d870a7
Christian Oder <myself5@carbonrom.org>
2020-01-08 09:58:41 -0500

    ambient: Update from taimen QQ1A.200105.002 build

--- ambient/Android.mk
+++ ambient/Android.mk
@@ -1,13 +1,1 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := AmbientSensePrebuilt
-LOCAL_MODULE_OWNER := ambientmusic
-LOCAL_SRC_FILES := proprietary/priv-app/AmbientSensePrebuilt/AmbientSensePrebuilt.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
--- ambient/ambient-vendor.mk
+++ ambient/ambient-vendor.mk
@@ -1,11 +1,11 @@
 PRODUCT_COPY_FILES += \
-    vendor/ambientmusic/ambient/proprietary/etc/ambient/matcher_tah.leveldb:system/etc/ambient/matcher_tah.leveldb \
-    vendor/ambientmusic/ambient/proprietary/etc/firmware/dnd.descriptor:system/etc/firmware/dnd.descriptor \
-    vendor/ambientmusic/ambient/proprietary/etc/firmware/dnd.sound_model:system/etc/firmware/dnd.sound_model \
-    vendor/ambientmusic/ambient/proprietary/etc/firmware/music_detector.descriptor:system/etc/firmware/music_detector.descriptor \
-    vendor/ambientmusic/ambient/proprietary/etc/firmware/music_detector.sound_model:system/etc/firmware/music_detector.sound_model \
-    vendor/ambientmusic/ambient/proprietary/etc/permissions/ambientsense.xml:system/etc/permissions/ambientsense.xml \
-    vendor/ambientmusic/ambient/proprietary/etc/sysconfig/ambientmusic.xml:system/etc/sysconfig/ambientmusic.xml
+    vendor/ambientmusic/ambient/proprietary/etc/firmware/dnd.descriptor:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/dnd.descriptor \
+    vendor/ambientmusic/ambient/proprietary/etc/firmware/dnd.sound_model:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/dnd.sound_model \
+    vendor/ambientmusic/ambient/proprietary/etc/firmware/music_detector.descriptor:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/music_detector.descriptor \
+    vendor/ambientmusic/ambient/proprietary/etc/firmware/music_detector.sound_model:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/music_detector.sound_model \
+    vendor/ambientmusic/ambient/proprietary/product/etc/ambient/matcher_tah.leveldb:$(TARGET_COPY_OUT_PRODUCT)/etc/ambient/matcher_tah.leveldb \
+    vendor/ambientmusic/ambient/proprietary/product/etc/permissions/ambientsense.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/ambientsense.xml \
+    vendor/ambientmusic/ambient/proprietary/product/etc/sysconfig/ambientmusic.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/ambientmusic.xml
 
 PRODUCT_PACKAGES += \
     AmbientSensePrebuilt
